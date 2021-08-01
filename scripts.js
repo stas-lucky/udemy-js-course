8c26dfa66bbe727342ca1a9de332e825b9368ab6
Stanislav Romashov <stanislav.romashov@simplifai.ai>
2021-08-02 01:00:33 +0200

    more ... 5

--- scripts.js
+++ scripts.js
@@ -3,24 +3,24 @@
 const set = 10;
 
 const calcAge = (birthday) => {
-  0;
-  console.log("23");
-  console.log();
+    0;
+    console.log("23");
+    console.log();
 };
 
 calcAge();
 
 const measureKelvin = function () {
-  const measurement = {
-    type: "temp",
-    unit: "celisus",
-    value: prompt("Degrees celcius: "),
-  };
+    const measurement = {
+        type: "temp",
+        unit: "celisus",
+        value: 10, //prompt("Degrees celcius: "),
+    };
 
-  console.log(measurement);
+    console.log(measurement);
 
-  const kelvin = Number(measurement.value) + 273;
-  return kelvin;
+    const kelvin = Number(measurement.value) + 273;
+    return kelvin;
 };
 
 console.log(measureKelvin());
